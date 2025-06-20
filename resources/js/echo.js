d3dda0ad665cb6f27cb4e5d8634ce393385b4338
Vaidas393 <37333650+Vaidas393@users.noreply.github.com>
2025-06-20 15:04:38 +0300

    Update echo.js

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -4,12 +4,11 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT,
-    wssPort: import.meta.env.VITE_REVERB_PORT,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    wsPath: '/app',
-    enabledTransports: ['ws', 'wss'],
+   broadcaster: 'reverb',
+   key: import.meta.env.VITE_REVERB_APP_KEY,
+   wsHost: import.meta.env.VITE_REVERB_HOST,
+   wsPort: 8081,
+   wssPort: 8081,
+   forceTLS: true,
+   enabledTransports: ['ws', 'wss'],
 });
